3f12b7ee1d7badbf9159dd432193034e917c71e3
土豆电工 <tudoudiangong@163.com>
2019-12-10 07:26:33 +0800

    ..

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Sign
+{
+    partial class Sign
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.textBoxSignText = new System.Windows.Forms.TextBox();
+            this.buttonSign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(12, 12);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(776, 214);
+            this.textBoxText.TabIndex = 0;
+            // 
+            // textBoxSignText
+            // 
+            this.textBoxSignText.Location = new System.Drawing.Point(12, 324);
+            this.textBoxSignText.Multiline = true;
+            this.textBoxSignText.Name = "textBoxSignText";
+            this.textBoxSignText.Size = new System.Drawing.Size(776, 134);
+            this.textBoxSignText.TabIndex = 1;
+            // 
+            // buttonSign
+            // 
+            this.buttonSign.Location = new System.Drawing.Point(12, 244);
+            this.buttonSign.Name = "buttonSign";
+            this.buttonSign.Size = new System.Drawing.Size(776, 65);
+            this.buttonSign.TabIndex = 2;
+            this.buttonSign.Text = "Sign";
+            this.buttonSign.UseVisualStyleBackColor = true;
+            this.buttonSign.Click += new System.EventHandler(this.buttonSign_Click);
+            // 
+            // Sign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.buttonSign);
+            this.Controls.Add(this.textBoxSignText);
+            this.Controls.Add(this.textBoxText);
+            this.Name = "Sign";
+            this.Text = "Sign for RSA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.TextBox textBoxSignText;
+        private System.Windows.Forms.Button buttonSign;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sign
+{
+    public partial class Sign : Form
+    {
+        public Sign()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSign_Click(object sender, EventArgs e)
+        {
+            textBoxSignText.Text = sign(textBoxText.Text);
+        }
+
+        public string sign(string str)
+        {
+            //根据需要加签时的哈希算法转化成对应的hash字符节
+            byte[] bt = Encoding.GetEncoding("utf-8").GetBytes(str);
+            var sha256 = new SHA256CryptoServiceProvider();
+            byte[] rgbHash = sha256.ComputeHash(bt);
+            RSACryptoServiceProvider key = new RSACryptoServiceProvider();
+            using (StreamReader streamReader = new StreamReader("PrivateKey.xml")) // 读取运行目录下的PrivateKey.xml
+            {
+                key.FromXmlString(streamReader.ReadToEnd());// 将私匙载入进RSA实例中
+            }
+            RSAPKCS1SignatureFormatter formatter = new RSAPKCS1SignatureFormatter(key);
+            formatter.SetHashAlgorithm("SHA256");//此处是你需要加签的hash算法，需要和上边你计算的hash值的算法一致，不然会报错。
+            byte[] inArray = formatter.CreateSignature(rgbHash);
+            return Convert.ToBase64String(inArray);
+        }
+    }
+}
